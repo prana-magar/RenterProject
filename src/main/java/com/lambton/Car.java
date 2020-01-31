ae354c6877b31b6ca3669ebcdcd722aa04c8ea94
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-01-31 08:57:45 -0500

    Added Class Car with constructor

---
+++ src/main/java/com/lambton/Car.java
@@ -0,0 +1,15 @@
+package com.lambton;
+
+public class Car extends Vehicle {
+
+    public String carType;
+    public String carColor;
+
+
+    public Car(String vehicleId, String description, String manufacturer, String carType, String carColor, Boolean isSelfDrive, Boolean isInsured, String insuranceProviderName, int numberOfSeat, FuelType fuelType, float baseRate) {
+        super(vehicleId, description, manufacturer, isSelfDrive, isInsured, insuranceProviderName, numberOfSeat, fuelType, baseRate);
+        this.carType = carType;
+        this.carColor = carColor;
+    }
+}
+
