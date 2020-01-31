bc1c5a7ce6d25a871f91fb576e8e8c2ff56103c5
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-01-31 09:10:16 -0500

    Added Class MotorCycle, addition of method pending

--- src/main/java/com/lambton/Car.java
+++ src/main/java/com/lambton/Car.java
@@ -5,7 +5,6 @@ public class Car extends Vehicle {
     public String carType;
     public String carColor;
 
-
     public Car(String vehicleId, String description, String manufacturer, String carType, String carColor, Boolean isSelfDrive, Boolean isInsured, String insuranceProviderName, int numberOfSeat, FuelType fuelType, float baseRate) {
         super(vehicleId, description, manufacturer, isSelfDrive, isInsured, insuranceProviderName, numberOfSeat, fuelType, baseRate);
         this.carType = carType;
