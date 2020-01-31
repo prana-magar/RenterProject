88e7c61d792299a2e60bc0d27ee5acc3736f8c15
prana <prakashrana9847067614@gmail.com>
2020-01-31 15:17:48 -0500

    [Prakash] Added vehicles in owner

--- src/main/java/com/lambton/Owner.java
+++ src/main/java/com/lambton/Owner.java
@@ -2,18 +2,22 @@ package com.lambton;
 
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Owner extends Person {
     private String companyTitle;
     private String landLineNumber;
     private String url;
+    private ArrayList<Vehicle> vehicles;
 
-    public Owner(String id, String firstName, String lastName, Gender gender, LocalDate birthDate, String mobileNumber, String emailId, String userName, String password, String companyTitle, String landLineNumber, String url) {
+    public Owner(String id, String firstName, String lastName, Gender gender, LocalDate birthDate, String mobileNumber, String emailId, String userName, String password, String companyTitle, String landLineNumber, String url, ArrayList<Vehicle> vehicles) {
         super(id, firstName, lastName, gender, birthDate, mobileNumber, emailId, userName, password);
         this.companyTitle = companyTitle;
         this.landLineNumber = landLineNumber;
         this.url = url;
+        this.vehicles = vehicles;
     }
+
 
     @Override
     public String toString() {
@@ -21,6 +25,7 @@ public class Owner extends Person {
                 super.toString()+
                 " companyTitle='" + companyTitle + '\'' +
                 ", landLineNumber='" + landLineNumber + '\'' +
+                ", Vehicles='" + vehicles + '\'' +
                 ", url='" + url + '\'' +
                 '}';
     }
