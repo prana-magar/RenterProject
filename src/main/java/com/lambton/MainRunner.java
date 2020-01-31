1e48c2e03fbb1181916d4d990b11e497c786e3ca
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-01-31 10:17:45 -0500

    Added object car1 for testing

--- src/main/java/com/lambton/MainRunner.java
+++ src/main/java/com/lambton/MainRunner.java
@@ -7,5 +7,8 @@ public class MainRunner {
         Customer c1 = new Customer("1","Prakash","Rana",Gender.MALE, LocalDate.now(),"98123123","prakash@gmail.com","ranaprakash","asdq321e1231231",new Address());
         //c1.display();
 
+        Car car1 = new Car("WAUMF98K09A690525","Brakes don't always work, good luck","Honda","Sedan","Purple",true,true,"Lambton",4,FuelType.PETROL,100);
+       System.out.println(car1.toString());
+        // car1.display();
     }
 }
