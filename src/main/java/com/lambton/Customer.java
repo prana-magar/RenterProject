0baa21bf70691bf230c7e3aad55880a7d6ae5af7
prana <prakashrana9847067614@gmail.com>
2020-01-31 15:16:12 -0500

    [Prakash] Added vehicle rent in Customer class

--- src/main/java/com/lambton/Customer.java
+++ src/main/java/com/lambton/Customer.java
@@ -1,16 +1,19 @@
 package com.lambton;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Customer extends Person implements IDisplay{
 
     private Address address;
+    private ArrayList<VehicleRent> vehicleRents;
 
-    Customer(String id, String firstName, String lastName, Gender gender, LocalDate birthDate, String mobileNumber, String emailId, String userName, String password,Address address) {
+     Customer(String id, String firstName, String lastName, Gender gender, LocalDate birthDate, String mobileNumber, String emailId, String userName, String password, Address address, ArrayList<VehicleRent> vehicleRents) {
         super(id, firstName, lastName, gender, birthDate, mobileNumber, emailId, userName, password);
         this.address = address;
-
+        this.vehicleRents = vehicleRents;
     }
+
 
     public void display(){
         System.out.println(this.toString());
@@ -22,6 +25,7 @@ public class Customer extends Person implements IDisplay{
         return "Customer{" +
                 super.toString()+
                 " address=" + address +
+                " vehicleRents=" + vehicleRents +
                 '}';
     }
 }
--- src/main/java/com/lambton/MainRunner.java
+++ src/main/java/com/lambton/MainRunner.java
@@ -1,11 +1,14 @@
 package com.lambton;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class MainRunner {
     public static void main(String[] args) {
-        Customer c1 = new Customer("1","Prakash","Rana",Gender.MALE, LocalDate.now(),"98123123","prakash@gmail.com","ranaprakash","asdq321e1231231",new Address());
-        //c1.display();
+
+        ArrayList<VehicleRent> vehicleRents = new ArrayList<>();
+        Customer c1 = new Customer("1","Prakash","Rana",Gender.MALE, LocalDate.now(),"98123123","prakash@gmail.com","ranaprakash","asdq321e1231231",new Address(),vehicleRents);
+        c1.display();
 
         Car car1 = new Car("WAUMF98K09A690525","Brakes don't always work, good luck","Honda","Sedan","Purple",true,true,"Lambton",4,FuelType.PETROL,100);
          car1.display();
