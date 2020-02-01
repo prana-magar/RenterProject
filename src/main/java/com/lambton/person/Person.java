98ecda97ba412c68679ffd7bc19b987c3fd54f3c
MacStudent <macstudent@128-iMAC03.local>
2020-01-31 20:42:07 -0500

    [Abhishek]Added Getter and Setter for Person Class

--- src/main/java/com/lambton/person/Person.java
+++ src/main/java/com/lambton/person/Person.java
@@ -53,4 +53,84 @@ import java.time.LocalDate;
                ", password='" + password + '\'' +
                '}';
     }
+
+    public String getId() {
+       return id;
+    }
+
+    public void setId(String id) {
+       this.id = id;
+    }
+
+    public String getFirstName() {
+       return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+       this.firstName = firstName;
+    }
+
+    public String getLastName() {
+       return lastName;
+    }
+
+    public void setLastName(String lastName) {
+       this.lastName = lastName;
+    }
+
+    public Gender getGender() {
+       return gender;
+    }
+
+    public void setGender(Gender gender) {
+       this.gender = gender;
+    }
+
+    public LocalDate getBirthDate() {
+       return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+       this.birthDate = birthDate;
+    }
+
+    public int getAge() {
+       return age;
+    }
+
+    public void setAge(int age) {
+       this.age = age;
+    }
+
+    public String getMobileNumber() {
+       return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+       this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmailId() {
+       return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+       this.emailId = emailId;
+    }
+
+    public String getUserName() {
+       return userName;
+    }
+
+    public void setUserName(String userName) {
+       this.userName = userName;
+    }
+
+    public String getPassword() {
+       return password;
+    }
+
+    public void setPassword(String password) {
+       this.password = password;
+    }
  }
