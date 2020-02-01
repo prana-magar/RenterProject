3facf7745740812ca89a6217426b6e958027d354
Raghav Bobal <raghavbobal@Raghavs-MacBook-Pro.local>
2020-01-31 20:46:04 -0500

    [Raghav] removed extra param int

--- src/main/java/com/lambton/person/Driver.java
+++ src/main/java/com/lambton/person/Driver.java
@@ -11,7 +11,7 @@ public class Driver extends Person implements IDisplay {
     private Float salary;
 
 
-    public Driver(String id, String firstName, String lastName, Gender gender, LocalDate birthDate, int age, String mobileNumber, String emailId, String userName, String password, String drivingLicenceNumber, Boolean isDrivingHistoryClean, Float salary) {
+    public Driver(String id, String firstName, String lastName, Gender gender, LocalDate birthDate, String mobileNumber, String emailId, String userName, String password, String drivingLicenceNumber, Boolean isDrivingHistoryClean, Float salary) {
         super(id, firstName, lastName, gender, birthDate, mobileNumber, emailId, userName, password);
         this.drivingLicenceNumber = drivingLicenceNumber;
         this.isDrivingHistoryClean = isDrivingHistoryClean;
