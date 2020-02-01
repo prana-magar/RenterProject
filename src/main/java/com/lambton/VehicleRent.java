9d07d25ff44a27edabc770b1d6be94f46a134c1c
MacStudent <macstudent@128-iMAC03.local>
2020-01-31 21:06:10 -0500

    Added Getter and Setter to VehicleRent Class

--- src/main/java/com/lambton/VehicleRent.java
+++ src/main/java/com/lambton/VehicleRent.java
@@ -29,4 +29,52 @@ public class VehicleRent implements IDisplay {
     public void display() {
         System.out.println(this.toString());
     }
+
+    public LocalDate getRentStartDate() {
+        return rentStartDate;
+    }
+
+    public void setRentStartDate(LocalDate rentStartDate) {
+        this.rentStartDate = rentStartDate;
+    }
+
+    public LocalDate getRentEndDate() {
+        return rentEndDate;
+    }
+
+    public void setRentEndDate(LocalDate rentEndDate) {
+        this.rentEndDate = rentEndDate;
+    }
+
+    public int getNumberOfDays() {
+        return numberOfDays;
+    }
+
+    public void setNumberOfDays(int numberOfDays) {
+        this.numberOfDays = numberOfDays;
+    }
+
+    public int getKmDrived() {
+        return kmDrived;
+    }
+
+    public void setKmDrived(int kmDrived) {
+        this.kmDrived = kmDrived;
+    }
+
+    public float getTotalBill() {
+        return totalBill;
+    }
+
+    public void setTotalBill(float totalBill) {
+        this.totalBill = totalBill;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
 }
