ef11e95dde558e549b8cc20af5709d299e2b3d64
raghavbobal1 <Shivji7&>
2020-02-02 16:23:42 -0500

    new class variables

--- src/main/java/com/lambton/util/PasswordUtil.java
+++ src/main/java/com/lambton/util/PasswordUtil.java
@@ -1,7 +1,15 @@
 package com.lambton.util;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
 /**
  * This class is to encrypt and validate the password
  */
 public class PasswordUtil {
+    private static final Random RANDOM = new SecureRandom();
+    private static final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final int ITERATIONS = 10000;
+    private static final int KEY_LENGTH = 256;
+
 }
