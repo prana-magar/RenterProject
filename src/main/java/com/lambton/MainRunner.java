2a0e8ce94825b9653c3ef8b8b076e87a288bc8f1
prana <prakashrana9847067614@gmail.com>
2020-02-01 21:31:01 -0500

    [Prakash] Added few queries

--- src/main/java/com/lambton/MainRunner.java
+++ src/main/java/com/lambton/MainRunner.java
@@ -1,15 +1,13 @@
 package com.lambton;
 
 import com.google.gson.Gson;
-import com.lambton.person.Customer;
-import com.lambton.person.Driver;
-import com.lambton.person.Gender;
-import com.lambton.person.Owner;
+import com.lambton.person.*;
 import com.lambton.util.Address;
 import com.lambton.vehicle.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainRunner {
 
@@ -26,43 +24,33 @@ public class MainRunner {
     static void init(){
         //Customer 1
         Customer c1 = new Customer("C1", "Prakash", "Rana", Gender.MALE, LocalDate.now(), "(437) 9908989 ", "prakash@gmail.com", "ranaprakash", "asdq321e1231231", new Address());
-        c1.display();
 
         //Customer 2
         Customer c2 = new Customer("C2", "Abhishek", "Jaya", Gender.MALE, LocalDate.now(), "(437) 9807865", "abhisheksj@gmail.com", "abysj", "abhishek199566", new Address());
-        c2.display();
 
         //Customer 3
         Customer c3 = new Customer("C3", "Raghav", "Bobal", Gender.MALE, LocalDate.now(), "(437) 6784597", "raghav21@gmail.com", "raghav22", "raghav77", new Address());
-        c3.display();
 
         //Customer 4
         Customer c4 = new Customer("C4", "Ozzy", "Osbourne", Gender.MALE, LocalDate.now(), "(437) 9807678", "therealozzy@gmail.com", "OzzyOsbourne", "ozzy@1249", new Address());
-        c4.display();
 
         //Customer 5
         Customer c5 = new Customer("C5", "Alicia", "Keys", Gender.FEMALE, LocalDate.now(), "(989) 9805690", "alicia@gmail.com", "AlKeys", "alicia22", new Address());
-        c5.display();
 
         //Driver 1
         Driver d1 = new Driver("D1", "Ram", "Rana", Gender.MALE, LocalDate.now(), "(989) 9076891", "ramrana@yahoo.com", "RamRana", "rananana11", "677-43-0205", Boolean.TRUE, 32332.6F);
-        d1.display();
 
         //Driver2
         Driver d2 = new Driver("D2", "Scott", "Adkins", Gender.MALE, LocalDate.now(), "(437) 9870091", "scottadkins@asd.com", "Scotty", "sctt77", "885-66-0755", Boolean.TRUE, 28099.6F);
-        d2.display();
 
         //Driver 3
         Driver d3 = new Driver("D3", "Charles", "Manson", Gender.MALE, LocalDate.now(), "(437) 8790991", "charlesmanson@asd.com", "CharlesManson", "charles44", "796-04-3290", Boolean.FALSE, 19090.0F);
-        d3.display();
 
         //Driver 4
         Driver d4 = new Driver("D4", "Caria", "MacNamee", Gender.FEMALE, LocalDate.now(), "(878) 7670009", "cmacnee@asd.com", "CMac", "cmanee090", "255-69-1229", Boolean.TRUE, 32019.0F);
-        d4.display();
 
         //Driver 5
         Driver d5 = new Driver("D5", "Eveline", "Lumox", Gender.FEMALE, LocalDate.now(), "(863) 2253855", "lumoxine@asd.com", "Elumonin", "lumox1992", "335-87-9680", Boolean.TRUE, 32231.0F);
-        d4.display();
 
         //Driver 6
         Driver d6 = new Driver("D6",
@@ -77,7 +65,6 @@ public class MainRunner {
                 "335-87-9620",
                 Boolean.TRUE,
                 32231.0F);
-        d4.display();
 
 
 
@@ -95,7 +82,6 @@ public class MainRunner {
                 FuelType.PETROL,
                 100,
                 d1);
-        car1.display();
 
         Car car2 = new Car("1GYS3HEF6ER890299",
                 "2020 Porsche 911",
@@ -108,7 +94,6 @@ public class MainRunner {
                 2,
                 FuelType.PETROL,
                 100);
-        car2.display();
 
         Car car3 = new Car("1D7RB1CP8AS240571",
                 "2018 Prius V",
@@ -121,7 +106,6 @@ public class MainRunner {
                 4,
                 FuelType.ELECTRIC,
                 100,d2);
-        car3.display();
 
         Car car4 = new Car("WBAWL1C57AP711666",
                 "1969 Ford Mustang",
@@ -134,7 +118,6 @@ public class MainRunner {
                 4,
                 FuelType.DIESEL,
                 100);
-        car4.display();
 
         Car car5 = new Car("5TDBKRFH4ES020941",
                 "2020 Mercedes-Benz GLE SUV",
@@ -147,7 +130,6 @@ public class MainRunner {
                 6,
                 FuelType.DIESEL,
                 100);
-        car5.display();
 
         Bus bus1 = new Bus("SALVN2BG3DH848046",
                 "Low-floor",
@@ -163,7 +145,6 @@ public class MainRunner {
                 FuelType.DIESEL,
                 250,
                 d3);
-        bus1.display();
 
         //Bus Objects
         Bus bus2 = new Bus("1B3BD2FB3BN077941",
@@ -180,7 +161,6 @@ public class MainRunner {
                 FuelType.DIESEL,
                 250,
                 d4);
-        bus2.display();
 
         Bus bus3 = new Bus("JM3KE2BE1F0888904",
                 "Minibus",
@@ -195,7 +175,6 @@ public class MainRunner {
                 false,
                 FuelType.DIESEL,
                 250);
-        bus3.display();
 
         Bus bus4 = new Bus("1G6YV34A945415578",
                 "Double-decker",
@@ -240,7 +219,6 @@ public class MainRunner {
                 50,
                 d6
         );
-        mc1.display();
 
         MotorCycle mc2 = new MotorCycle("2C3CDZAG8FH338790",
                 "2019 Scrambler",
@@ -253,7 +231,6 @@ public class MainRunner {
                 26,
                 135,
                 50);
-        mc2.display();
 
         MotorCycle mc3 = new MotorCycle("1FTEW1E82AF943821",
                 "2004 Thundercat YZF600R",
@@ -266,7 +243,6 @@ public class MainRunner {
                 43,
                 145,
                 50);
-        mc3.display();
         //Vehicle objects END here
 
         //List of vehicles and information of first owner
@@ -274,21 +250,18 @@ public class MainRunner {
         vehicles1.add(car1);
         vehicles1.add(car2);
         Owner o1 = new Owner("1", "Odetta", "Vlasov", Gender.FEMALE, LocalDate.now(), "(463) 6723367", "odetta@gmail.com", "odettavlasov", "ode22", "Quick Motors", "8127289453", "quickmotors.com", vehicles1);
-        o1.display();
 
         //List of vehicles and information of second owner
         ArrayList<Vehicle> vehicles2 = new ArrayList<Vehicle>();
         vehicles2.add(car3);
         vehicles2.add(bus1);
         Owner o2 = new Owner("2", "Emilia", "Clarke", Gender.FEMALE, LocalDate.now(), "(463) 4263352", "emiliac@gmail.com", "emc", "emilia7%66", "Independent", "8127456713", "emiliascars.com", vehicles2);
-        o2.display();
 
         //List of vehicles and information of third owner
         ArrayList<Vehicle> vehicles3 = new ArrayList<Vehicle>();
         vehicles3.add(car4);
         vehicles3.add(bus2);
         Owner o3 = new Owner("3", "Simon", "Cowell", Gender.MALE, LocalDate.now(), "(689) 51263458", "simontheman@gmail.com", "SimonCl", "password", "Indus Dealers", "812245789", "indusdealers.com", vehicles3);
-        o3.display();
 
         //List of vehicles and information of fourth owner
         ArrayList<Vehicle> vehicles4 = new ArrayList<Vehicle>();
@@ -297,14 +270,12 @@ public class MainRunner {
         vehicles4.add(bus4);
         vehicles4.add(mc2);
         Owner o4 = new Owner("4", "Frederick", "Mercury", Gender.MALE, LocalDate.now(), "(689) 36763478", "queen@gmail.com", "Fmerc", "qwerty", "Feels on Wheels", "907645909", "feelsonwheels.com", vehicles4);
-        o4.display();
 
         //List of vehicles and information of fifth owner
         ArrayList<Vehicle> vehicles5 = new ArrayList<Vehicle>();
         vehicles5.add(mc1);
         vehicles5.add(bus5);
         Owner o5 = new Owner("5", "Jeff", "Cavaliere", Gender.MALE, LocalDate.now(), "(689) 44665590", "jeffcavaliere@gmail.com", "Jeff", "abc123", "AthleanX Motors", "890459789", "athleanx.com", vehicles5);
-        o5.display();
 
 
         //VehicleRents objects and addition to customer
@@ -336,7 +307,7 @@ public class MainRunner {
         LocalDate startDate6 = LocalDate.of(2019,1,25);
         LocalDate endDate6 = startDate6.plusDays(1);
         VehicleRent vehicleRent6 = new VehicleRent(startDate6,endDate6,mc3);
-        c5.addVehicleRent(vehicleRent6);
+        c4.addVehicleRent(vehicleRent6);
 
         customers = new ArrayList<>();
         customers.add(c1);
@@ -389,7 +360,29 @@ public class MainRunner {
     }
 
 
+
     public static void main(String[] args) {
         init();
+
+        // Print all customers
+        for(Person person: customers){
+            person.display();
+        }
+
+        // Print all owners
+        for(Owner owner: owners){
+            owner.display();
+        }
+
+        // all customers with some bookings
+        for(Customer customer: customers){
+            if(customer.getVehicleRents() != null){
+                customer.display();
+            }
+        }
+
+
+
+
     }
 }
